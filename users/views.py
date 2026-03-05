3bc5a4753dd6a9c9a0ef16d871e49d3ef73bafd1
luis rios <lrioscoutino@gmail.com>
2026-03-04 20:52:17 -0600

    List Products

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from users.models import Product
 
+
 # Create your views here.
 def get_user(request):
     return HttpResponse("Hello World")
@@ -10,10 +11,13 @@ def get_user(request):
 def inicio(request):
     products = Product.objects.all()
     context = {
-        "name":"Roberto Carlos",
-        "email":"r@gmail.com",
-        "age": 35,
-        "resumen":[3,5,67,7,8,8],
         "products":products
     }
     return render(request, "base.html",context=context)
+
+def acerca_de(request):
+    return render(request, "acerca_de.html")
+
+def list_products(request):
+    products = Product.objects.all()
+    return render(request, "products.html",context={"products":products})
